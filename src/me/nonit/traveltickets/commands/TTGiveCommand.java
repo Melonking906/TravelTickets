1902f2463fd41c765cf271d157c89f9313b24805
Daniel Murray <daniel@nonit.me>
2014-10-16 13:20:09 +0100

    Added give cost option.

--- src/me/nonit/traveltickets/commands/TTGiveCommand.java
+++ src/me/nonit/traveltickets/commands/TTGiveCommand.java
@@ -28,7 +28,7 @@ public class TTGiveCommand extends TTSubCommand
 
         if( args.length < 7 )
         {
-            sender.sendMessage( PREFIX + "Give a travel ticket with " + ChatColor.YELLOW + "/" + commandLabel + " give <player> <name> <world> <x> <y> <z> (amount)" + ChatColor.GREEN + "!" );
+            sender.sendMessage( PREFIX + "Give a travel ticket with " + ChatColor.YELLOW + "/" + commandLabel + " give <player> <name> <world> <x> <y> <z> (amount) (cost)" + ChatColor.GREEN + "!" );
             return true;
         }
 
@@ -44,7 +44,8 @@ public class TTGiveCommand extends TTSubCommand
 
         if( i.firstEmpty() == -1 )
         {
-            p.sendMessage( PREFIX + ChatColor.RED + "Sorry but " + ChatColor.WHITE + p.getName() + ChatColor.RED + " has a full inventory!" );
+            sender.sendMessage( PREFIX + ChatColor.RED + "Sorry but " + ChatColor.WHITE + p.getName() + ChatColor.RED + " has a full inventory!" );
+            p.sendMessage( PREFIX + ChatColor.RED + sender.getName() + " tried to give you a ticket but your inventory is full :(" );
             return true;
         }
 
@@ -54,6 +55,7 @@ public class TTGiveCommand extends TTSubCommand
         double y;
         double z;
         int quantity = 1;
+        double cost = 0;
 
         try
         {
@@ -66,16 +68,36 @@ public class TTGiveCommand extends TTSubCommand
             {
                 quantity = Integer.parseInt( args[7] );
             }
+            if( args.length > 8 )
+            {
+                String costArg = args[8];
+                if( costArg.equals( "default" ) )
+                {
+                    cost = COST * quantity;
+                }
+                else
+                {
+                    cost = Double.parseDouble( costArg ) * quantity;
+                }
+            }
         }
         catch( Exception e )
         {
-            sender.sendMessage( PREFIX + ChatColor.RED + "Sorry but there was an error in that location! Sure its correct?" );
+            sender.sendMessage( PREFIX + ChatColor.RED + "Sorry but there was an error in your arguments! Sure its correct?" );
             return true;
         }
 
         if( world == null )
         {
             sender.sendMessage( PREFIX + ChatColor.RED + "Erm.. there is no world called " + ChatColor.WHITE + args[3] + ChatColor.RED +" on this server!" );
+            return true;
+        }
+
+        if( cost != 0 && ECONOMY.getBalance( p ) < cost )
+        {
+            sender.sendMessage( PREFIX + ChatColor.RED + "Sadly " + p.getDisplayName() + " cant afford to pay " + ChatColor.WHITE + ECONOMY.format( cost ) + ChatColor.RED + "!" );
+
+            p.sendMessage( PREFIX + ChatColor.RED + sender.getName() + " tried to charge you " + ChatColor.WHITE + ECONOMY.format( cost ) + ChatColor.RED + " for a ticket, but you only have " + ChatColor.WHITE + ECONOMY.getBalance( p ) + ChatColor.RED + " :(" );
             return true;
         }
 
@@ -88,6 +110,19 @@ public class TTGiveCommand extends TTSubCommand
 
         sender.sendMessage( PREFIX + "Success! You gave a ticket called " + ChatColor.YELLOW + name + ChatColor.GREEN + " to " + ChatColor.YELLOW + p.getDisplayName() + ChatColor.GREEN + "!" );
         p.sendMessage( PREFIX + "You received a travel ticket to " + ChatColor.YELLOW + name + ChatColor.GREEN + " from " + ChatColor.YELLOW + sender.getName() + ChatColor.GREEN + "!" );
+
+        if( cost != 0 && ! p.hasPermission( "traveltickets.free" ) )
+        {
+            ECONOMY.withdrawPlayer( p, cost );
+
+            if( ! TICKET_ACCOUNT.equals( "" ) )
+            {
+                ECONOMY.depositPlayer( TICKET_ACCOUNT, cost );
+            }
+
+            p.sendMessage( PREFIX + "You were charged " + ChatColor.YELLOW + ECONOMY.format( cost ) + ChatColor.GREEN +
+                    " for ticket(s) to " + ChatColor.YELLOW + name + ChatColor.GREEN + "!" );
+        }
 
         return true;
     }
